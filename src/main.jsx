311e794a81780b26bffd5d2cc70379fb5f33f9b3
El Hudson <ehudson@wellesley.edu>
2023-10-23 20:23:29 -0400

    spa fix mayb?

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,17 @@ import App from './App.jsx'
 
 import { pdfjs } from 'react-pdf';
 
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location))
+
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
   import.meta.url,
