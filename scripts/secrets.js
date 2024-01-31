36915012011bc4e3492f02d626edc9747da693ad
El Hudson <67842840+elhudson@users.noreply.github.com>
2024-01-31 10:08:04 -0500

    api keys

--- scripts/secrets.js
+++ scripts/secrets.js
@@ -4,6 +4,8 @@ var args = process.argv.slice(2);
 fs.writeFileSync(
   `env.json`,
   JSON.stringify({
-    github: args[0]
+    github: args[0],
+    spotify: JSON.parse(args[1]),
+    omnivore: args[2]
   })
 );
